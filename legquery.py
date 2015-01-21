0bfd63000ecb5d7e9d65777693553cb5b719ec1a
githamm <hamm.kevin@gmail.com>
2015-01-20 18:31:47 -0700

    Write the bill list to a file

--- legquery.py
+++ legquery.py
@@ -4,5 +4,8 @@ import json
 
 if __name__ == '__main__':
 	co_bill = sunlight.openstates.bills(state='co')
-	json.dumps(co_bill)
+	# print json.dumps(co_bill)
+	fh = open('co-bills.json', 'wb')
+	json.dump(co_bill, fh)
+
 
