bf4ad51d7cb6a8acacede6e3eee34cc5ba86c009
freejoe76 <joe.murphy@gmail.com>
2015-01-23 10:21:39 -0500

    Fix tabs

--- legquery.py
+++ legquery.py
@@ -5,10 +5,10 @@ import os
 
 
 if __name__ == '__main__':
-	co_bill = sunlight.openstates.bills(state='co')
-	# print json.dumps(co_bill)
-	fh = open('co-bills.json', 'wb')
-	json.dump(co_bill, fh)
+    co_bill = sunlight.openstates.bills(state='co')
+    # print json.dumps(co_bill)
+    fh = open('co-bills.json', 'wb')
+    json.dump(co_bill, fh)
     # We'll need to store the files we're writing somewhere, eventually.
     directory = os.path.dirname(os.path.realpath(__file__))
     if not os.path.isdir('%s/output' % directory):
