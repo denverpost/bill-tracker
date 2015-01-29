ddc91699dcee9ee4e0f9249964304f1416637b8c
freejoe76 <joe.murphy@gmail.com>
2015-01-28 20:43:00 -0500

    Added some boilerplate for pytests

---
+++ tests/test_legquery.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import pytest
+import legquery
+
+'''
+def test_publish():
+    """ Test the Sheet's publish method.
+        """
+    sheet = Sheet('test-sheet', 'worksheet-name')
+    publish_value = sheet.publish()
+    assert publish_value == True
+
+def test_filters():
+    """ Test adding a filter to the sheet.
+        """
+    sheet = Sheet('test-sheet', 'worksheet-name')
+    sheet.add_filter('name', 'test')
+    assert sheet.filters == [{'value': 'test', 'key': 'name'}]
+
+def test_filename():
+    """ Test the build_filename() method.
+        """
+    sheet = Sheet('test-sheet', 'worksheet-name')
+    sheet.add_filter('name', 'test')
+    sheet.build_filename()
+    assert sheet.filename == 'worksheet-name-test'
+'''
