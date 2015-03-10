4920ede4e123bb926daeed6e2e720f9db79a694a
githamm <hamm.kevin@gmail.com>
2015-03-10 16:34:40 -0600

    cleanup

--- legquery.py
+++ legquery.py
@@ -12,10 +12,13 @@ class Sunlight:
         pass
 
 def main():
-    co_bill = sunlight.openstates.bills(state='co')
+    # co_bill = sunlight.openstates.bills(state='co')
     # print json.dumps(co_bill)
     fh = open('co-bills.json', 'wb')
-    json.dump(co_bill, fh)
+    fh = open('co-bills.json', 'rb')
+    co_bill = json.load(fh)
+    print co_bill[0]
+    #json.dump(co_bill, fh)
     # We'll need to store the files we're writing somewhere, eventually.
     directory = os.path.dirname(os.path.realpath(__file__))
     if not os.path.isdir('%s/output' % directory):
